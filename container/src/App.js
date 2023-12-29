dbdb06027dd5a3a826432169a95ab7602b11a57b
Karibasaveshwara_TG <karibasaveshwara.tg@moonraft.com>
2023-12-29 21:04:45 +0530

    revert back changes

--- container/src/App.js
+++ container/src/App.js
@@ -1,20 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import {
-  StylesProvider,
-  createGenerateClassname,
-} from "@material-ui/core/styles";
+import { StylesProvider } from "@material-ui/core/styles";
 
 import MarketingApp from "../components/MarketingApp";
 import Header from "../components/Header";
 
-const generateClassName = createGenerateClassname({
-  productionPrefix: "co",
-});
-
 export default () => {
   return (
-    <StylesProvider generateClassName={generateClassName}>
+    <StylesProvider>
       <BrowserRouter>
         <div>
           <Header />
