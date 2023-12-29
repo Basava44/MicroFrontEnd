1d9a2928e0bd800bbe45c282db739eb551daed8a
Karibasaveshwara_TG <karibasaveshwara.tg@moonraft.com>
2023-12-29 20:40:31 +0530

    generate specific class name for styles conflicts

--- container/src/App.js
+++ container/src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassname,
+} from "@material-ui/core/styles";
+
 import MarketingApp from "../components/MarketingApp";
 import Header from "../components/Header";
 
+const generateClassName = createGenerateClassname({
+  productionPrefix: "co",
+});
+
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
